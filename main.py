f801fe260f3b3659bc8bf1cb146d14c3497ae1af
Everly <66821749+everlyy@users.noreply.github.com>
2024-02-28 20:48:55 +0100

    Other Discord RPC library
    It's still not reliable if your Discord restarts or if the program
    starts before Discord

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import urllib.parse
 import StateHandler
 from typing import Callable
 import asyncio
+import discordrp
 
 app = flask.Flask(
 	__name__,
@@ -14,16 +15,72 @@ app = flask.Flask(
 	template_folder="web/templates"
 )
 
+class RPCWrapper:
+	def __init__(self, client_id):
+		self.client_id = client_id
+		self._rpc: (discordrp.Presence | None) = None
+
+	def _connect(self):
+		if self._rpc is None:
+			try:
+				self._rpc = discordrp.Presence(self.client_id)
+			except Exception as e:
+				print(f"Couldn't connect to Discord RPC: {e}")
+				self._rpc = None
+
+	def _is_connected(self):
+		if self._rpc is None:
+			self._connect()
+
+		return self._rpc is not None
+
+	def clear(self, *args, **kwargs):
+		if self._is_connected():
+			try:
+				return self._rpc.clear(*args, **kwargs)
+			except Exception as e:
+				print(f"Couldn't clear Discord RPC: {e}")
+				self._rpc = None
+
+	def set(self, *args, **kwargs):
+		if self._is_connected():
+			try:
+				return self._rpc.set(*args, **kwargs)
+			except Exception as e:
+				print(f"Couldn't clear Discord RPC: {e}")
+				self._rpc = None
+
+rpc = RPCWrapper(DISCORD_CLIENT_ID)
 state = StateHandler.State()
 
 @state.state_changed
 def state_changed(state: StateHandler.State):
 	track: (StateHandler.Track | None) = state.now_playing
 	if track is None:
-		# Clear RPC
+		rpc.clear()
 		return
 
-	# Set RPC
+	rpc_state = {
+		"details": track.title,
+		"state": f"by {track.artist}",
+		"assets": {
+			"small_image": "ws",
+			"small_text": f"{track.connector} connector"
+		},
+		"timestamps": {
+			"start": track.start_time
+		}
+	}
+
+	if track.cover is not None:
+		rpc_state["assets"]["large_image"] = track.cover
+	
+	if track.album is not None:
+		rpc_state["assets"]["large_text"] = track.album
+
+	print(rpc_state)
+
+	rpc.set(rpc_state)
 
 def generate_auth_url() -> str:
 	event_endpoint = flask.url_for("event")
