42c7c77a567ef7dfeec845862aeb271aea5d42b0
Everly <66821749+everlyy@users.noreply.github.com>
2024-03-07 23:36:46 +0100

    Fix title not changing back when pausing

--- app/web/static/scripts/fetch-playing.js
+++ app/web/static/scripts/fetch-playing.js
@@ -3,13 +3,13 @@ const fetch_interval = 2500;
 let fetch_playing = () => {
     fetch("/now-playing").then((response) => response.json()).then((json) => {
         NowPlaying.innerHTML = json.document;
+        document.title = (
+            json.state ?
+            `${json.state.artist} - ${json.state.title} | Web Scrobbler RPC` :
+            "Web Scrobbler RPC"
+        );
 
-        if(!json.state)
-            return;
-
-        document.title = `${json.state.artist} - ${json.state.title} | Web Scrobbler RPC`;
-
-        if(json.state.cover) {
+        if(json.state && json.state.cover) {
             Background.style.setProperty("--cover-image-url", `url("${json.state.cover}")`);
             Background.classList.add("has-cover");
         } else {
